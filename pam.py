1d91f1e3fd126b6c81bd34c5365009c5e4d25bfe
Lenival <lenival@ufrn.edu.br>
2020-10-09 13:32:00 -0300

    Create pam.py
    Raw M-PAM stream with length 'len' and variance 'Var' acording with PAM
    modulation signal model

---
+++ pam.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Oct  9 12:58:38 2020
+
+@author: LENI
+
+seq=pam(len,M,Var)
+Raw M-PAM stream with length 'len' and variance 'Var' acording with PAM
+modulation signal model.
+
+"""
+
+import numpy as np
+#import math
+
+def pam(len,M,Var):
+    return (2*np.floor(M*np.random.rand(1,len))-M+1)*np.sqrt(3*Var/(M^2-1))
+
+
+if __name__ == "__main__":
+    pam(10,2,1)

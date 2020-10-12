58f6ee8353f2997d96fe07d0fb395da683d2a662
Lenival <lenival@ufrn.edu.br>
2020-10-12 17:00:07 -0300

    Update srrc.py
    Removed unnecessary code

--- srrc.py
+++ srrc.py
@@ -19,22 +19,16 @@ import warnings
 
 
 def srrc(syms, beta, P, t_off=0):
-    t=np.arange(-syms*P+t_off,syms*P+1+t_off)            # sampling indices as a multiple of T/P
-    """
-    if (beta==0):
-        beta=np.finfo(float).tiny                     # numerical problems if beta=0
-    
-    return (4*beta/np.sqrt(P)*(np.cos((1+beta)*np.pi*t/P)+\
-    np.sin((1-beta)*np.pi*t/P)/(4*beta*t/P))/\
-    (np.pi*(1-16*(beta*t/P)**2)))              # calculation of srrc pulse
-    """
+    # sampling indices as a multiple of T/P
+    t=np.arange(-syms*P+t_off,syms*P+1+t_off)            
+
     #with warnings.catch_warnings():
     try:
         warnings.simplefilter("ignore")
+        # calculation of srrc pulse
         srrc_w = ((np.sin((1-beta)*np.pi*t/P)+\
                  (4*beta*t/P)*np.cos((1+beta)*np.pi*t/P))/\
-                (np.sqrt(P)*(np.pi*t/P)*(1-16*(beta*t/P)**2))) # calculation of srrc pulse
- #       warnings.simplefilter("default")
+                (np.sqrt(P)*(np.pi*t/P)*(1-16*(beta*t/P)**2)))
     finally:
         srrc_w[t==0] = (1-beta+4*beta/np.pi)/np.sqrt(P)     # replace when t=0
         if beta !=0 :
@@ -45,4 +39,4 @@ def srrc(syms, beta, P, t_off=0):
     return srrc_w
         
 if __name__ == "__main__":
-    print(srrc(3,0.1,2))
+    print(srrc(3,0,2))

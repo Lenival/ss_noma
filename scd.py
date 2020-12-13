1b44165a32af2c68c0944aed0d09ffd826cfc97d
Lenival <lenival@ufrn.edu.br>
2020-12-13 18:10:08 -0300

    Update scd.py
    Triangular SCD with nan where the surface values are repeated

--- scd.py
+++ scd.py
@@ -17,21 +17,27 @@ def scd(x):
     X = np.fft.fft(x)
     X_aux = (1/M)*np.conj(X)
     Sx = np.empty((M, M))
+    Sx[:,:] = np.NAN
     
-    for tau in range(M):
-        Sx[tau,:] = X*np.roll(X_aux,tau)        
+    for alpha in range(M):
+        i=int(np.ceil((alpha-1)/2))
+        f=M-int(np.floor((alpha+1)/2))
+        Sx[alpha,i:f] = X[alpha:]*X_aux[0:(M-alpha)]        
     
     return Sx
         
 if __name__ == "__main__":
+    """
     M = 512
-    x = np.random.random(M)
+    t = np.linspace(0,511,512)
+    x = np.sin(2*np.pi*(1/12)*t)+np.sin(2*np.pi*(1/16)*t)
+    #x = np.random.random(M)
     Sx = scd(x)
     alpha = np.linspace(0,M-1,M)
     f = np.linspace(0,M-1,M)
     alpha, f = np.meshgrid(alpha,f)
     fig = plt.figure()
     ax = fig.gca(projection = '3d')
-    ax.plot_surface(alpha,f,np.abs(Sx),cmap=cm.coolwarm)
+    ax.plot_surface(alpha,f,np.abs(Sx),cmap=cm.seismic)
     plt.show()
-    
+    """
